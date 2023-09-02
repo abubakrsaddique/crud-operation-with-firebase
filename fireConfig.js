4aba84bb3d4f21c5a71736cff6578224142a7675
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2023-09-02 16:28:42 +0500

    first commit

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAxPrg7YiSRwW3T5g3bHPbhii2_KfAJ0I",
+    authDomain: "crud-operation-fa612.firebaseapp.com",
+    databaseURL: "https://crud-operation-fa612-default-rtdb.firebaseio.com",
+    projectId: "crud-operation-fa612",
+    storageBucket: "crud-operation-fa612.appspot.com",
+    messagingSenderId: "396509799159",
+    appId: "1:396509799159:web:4f79d86852946aa8c1e9f2",
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
